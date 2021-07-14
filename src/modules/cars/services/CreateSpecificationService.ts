b5c9032bcd3df82be92e28862d7dacc7f2b140d2
Matheus Felipe Zanin <matt_z6@hotmail.com>
2021-07-14 15:33:30 -0300

    feat: add create specification service

---
+++ src/modules/cars/services/CreateSpecificationService.ts
@@ -0,0 +1,20 @@
+import { ISpecificationsRepository } from '../repositories/ISpecificationsRepository';
+
+type Request = {
+  name: string;
+  description: string;
+};
+
+export class CreateSpecificationService {
+  constructor(private specificationsRepository: ISpecificationsRepository) {}
+
+  execute({ name, description }: Request): void {
+    const specification = this.specificationsRepository.findByName(name);
+
+    if (specification) {
+      throw new Error('Specification already exists');
+    }
+
+    this.specificationsRepository.create({ name, description });
+  }
+}
