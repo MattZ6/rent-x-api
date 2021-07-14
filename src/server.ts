9a0ffbfc5a5fec9621957b32f611d9166963ad19
Matheus Felipe Zanin <matt_z6@hotmail.com>
2021-07-14 15:37:33 -0300

    feat: add specifications routes with create method

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,14 @@
 import express from 'express';
 
 import { categoriesRoutes } from './routes/categories.routes';
+import { specificationsRoutes } from './routes/specifications.routes';
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/v1/categories', categoriesRoutes);
+app.use('/v1/specifications', specificationsRoutes);
 
 app.listen(3333, () =>
   console.log('Server is running at http://localhost:3333')
