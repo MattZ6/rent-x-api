13955bbcc95474c29facfc3c5dc1ce56eefa2d6e
Matheus Felipe Zanin <matt_z6@hotmail.com>
2021-07-15 11:09:13 -0300

    chore(categories): add import route

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,7 +1,11 @@
 import { Router } from 'express';
+import multer from 'multer';
 
 import { createCategoryController } from '../modules/cars/use-cases/create-category';
+import { importCategoriesController } from '../modules/cars/use-cases/import-categories';
 import { listCategoriesController } from '../modules/cars/use-cases/list-categories';
+
+const upload = multer({ dest: './temp' });
 
 const categoriesRoutes = Router();
 
@@ -11,6 +15,10 @@ categoriesRoutes.post('/', (req, res) =>
 
 categoriesRoutes.get('/', (req, res) =>
   listCategoriesController.handle(req, res)
+);
+
+categoriesRoutes.post('/import', upload.single('file'), (req, res) =>
+  importCategoriesController.handle(req, res)
 );
 
 export { categoriesRoutes };
