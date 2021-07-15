ba73b0fbb387535fc6a9e5fc1ea3934fc0ceb77d
Matheus Felipe Zanin <matt_z6@hotmail.com>
2021-07-15 12:00:47 -0300

    chore(deps): add swagger

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,14 @@
 import express from 'express';
+import swaggerUI from 'swagger-ui-express';
 
+import swaggerFile from './docs/swagger.json';
 import { routes } from './routes';
 
 const app = express();
 
 app.use(express.json());
+
+app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerFile));
 
 app.use(routes);
 
