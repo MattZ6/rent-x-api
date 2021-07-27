3b86a6b67721ae4c7cbbd900bc39568827e5a3b1
Matheus Felipe Zanin <matt_z6@hotmail.com>
2021-07-27 20:44:14 -0300

    chore: add create specifications migration

---
+++ src/database/migrations/1627429074703-CreateSpecifications.ts
@@ -0,0 +1,42 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateSpecifications1627429074703 implements MigrationInterface {
+  private tableName = 'specifications';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: this.tableName,
+        columns: [
+          {
+            isPrimary: true,
+            name: 'id',
+            type: 'uuid',
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+          },
+          {
+            name: 'description',
+            type: 'varchar',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable(this.tableName);
+  }
+}
