361521b465462cc95c1cc180c241f413c39c15c2
Matheus Felipe Zanin <matt_z6@hotmail.com>
2021-07-28 22:04:44 -0300

    chore: add users repository implementation

---
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -0,0 +1,27 @@
+import { getRepository, Repository } from 'typeorm';
+
+import { CreateUserDTO } from '../../dtos/CreateUserDTO';
+import { User } from '../../entities/User';
+import { IUsersRepository } from '../IUserRepository';
+
+export class UsersRepository implements IUsersRepository {
+  private repository: Repository<User>;
+
+  constructor() {
+    this.repository = getRepository(User);
+  }
+
+  async create(data: CreateUserDTO): Promise<void> {
+    const { name, email, password, username, driver_license } = data;
+
+    const user = this.repository.create({
+      name,
+      email,
+      password,
+      username,
+      driver_license,
+    });
+
+    await this.repository.save(user);
+  }
+}
