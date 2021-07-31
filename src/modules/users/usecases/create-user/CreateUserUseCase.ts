7626d16daf44c20d009a5a7aca40e5443b0a43ce
Matheus Felipe Zanin <matt_z6@hotmail.com>
2021-07-31 17:39:55 -0300

    chore(CreateUserUseCase): hash password and prevent create user with email from another

--- src/modules/users/usecases/create-user/CreateUserUseCase.ts
+++ src/modules/users/usecases/create-user/CreateUserUseCase.ts
@@ -1,3 +1,4 @@
+import { hash } from 'bcryptjs';
 import { inject, injectable } from 'tsyringe';
 
 import { CreateUserDTO } from '../../dtos/CreateUserDTO';
@@ -13,10 +14,18 @@ export class CreateUserUseCase {
   async execute(data: CreateUserDTO): Promise<void> {
     const { name, email, password, driver_license } = data;
 
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (user) {
+      throw new Error('User already with this email');
+    }
+
+    const passwordHash = await hash(password, 8);
+
     await this.usersRepository.create({
       name,
       email,
-      password,
+      password: passwordHash,
       driver_license,
     });
   }
