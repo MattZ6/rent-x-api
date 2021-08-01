08e66442d33856c16d9e14a2a356dc2a5739fb65
Matheus Felipe Zanin <matt_z6@hotmail.com>
2021-07-31 21:33:19 -0300

    feat: add UpdateUserAvatarController

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,11 +1,28 @@
 import { Router } from 'express';
+import multer from 'multer';
 
+import uploadConfig from '../config/upload';
+import { ensureAuthenticated } from '../middlewares/ensure-authenticated';
 import { CreateUserController } from '../modules/users/usecases/create-user/CreateUserController';
+import { UpdateUserAvatarController } from '../modules/users/usecases/update-user-avatar/UpdateUserAvatarController';
 
 const routes = Router();
 
+const uploadAvatar = multer(uploadConfig.upload('./temp/avatar'));
+
 const createUserController = new CreateUserController();
+const updateUserAvatarController = new UpdateUserAvatarController();
 
 routes.post('/', createUserController.handle);
+
+/** Auth routes */
+
+routes.use(ensureAuthenticated);
+
+routes.patch(
+  '/avatar',
+  uploadAvatar.single('avatar'),
+  updateUserAvatarController.handle
+);
 
 export default routes;
