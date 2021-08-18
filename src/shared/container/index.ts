ba22a2864336efa22d637bf65048bddb6109bd29
Matheus Felipe Zanin <matt_z6@hotmail.com>
2021-08-18 11:58:32 -0300

    refactor(shared): update imports

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,11 +1,11 @@
 import { container } from 'tsyringe';
 
-import { ICategoriesRepository } from '../../modules/cars/repositories/ICategoriesRepository';
-import { CategoriesRepository } from '../../modules/cars/repositories/implementations/CategoriesRepository';
-import { SpecificationsRepository } from '../../modules/cars/repositories/implementations/SpecificationsRepository';
-import { ISpecificationsRepository } from '../../modules/cars/repositories/ISpecificationsRepository';
-import { UsersRepository } from '../../modules/users/repositories/implementations/UsersRepository';
-import { IUsersRepository } from '../../modules/users/repositories/IUserRepository';
+import { ICategoriesRepository } from '@modules/cars/repositories/ICategoriesRepository';
+import { CategoriesRepository } from '@modules/cars/repositories/implementations/CategoriesRepository';
+import { SpecificationsRepository } from '@modules/cars/repositories/implementations/SpecificationsRepository';
+import { ISpecificationsRepository } from '@modules/cars/repositories/ISpecificationsRepository';
+import { UsersRepository } from '@modules/users/repositories/implementations/UsersRepository';
+import { IUsersRepository } from '@modules/users/repositories/IUserRepository';
 
 container.registerSingleton<ICategoriesRepository>(
   'CategoriesRepository',
