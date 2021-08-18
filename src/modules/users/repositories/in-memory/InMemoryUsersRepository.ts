4b60b56d9bd193f27f019cc4a4e525029830b1d0
Matheus Felipe Zanin <matt_z6@hotmail.com>
2021-08-18 10:04:24 -0300

    chore(users): add in memory users repository

---
+++ src/modules/users/repositories/in-memory/InMemoryUsersRepository.ts
@@ -0,0 +1,44 @@
+import { CreateUserDTO } from '../../dtos/CreateUserDTO';
+import { User } from '../../entities/User';
+import { IUsersRepository } from '../IUserRepository';
+
+export class InMemoryUsersRepository implements IUsersRepository {
+  private users: User[] = [];
+
+  async create(data: CreateUserDTO): Promise<void> {
+    const { name, email, password, driver_license } = data;
+
+    const user = new User();
+
+    Object.assign(user, {
+      name,
+      email,
+      password,
+      driver_license,
+      created_at: new Date(),
+      updated_at: new Date(),
+    } as User);
+
+    this.users.push(user);
+  }
+
+  async update(data: User): Promise<void> {
+    this.users = this.users.map(user =>
+      user.id === data.id
+        ? {
+            ...user,
+            ...data,
+            updated_at: new Date(),
+          }
+        : user
+    );
+  }
+
+  async findById(id: string): Promise<User | undefined> {
+    return this.users.find(user => user.id === id);
+  }
+
+  async findByEmail(email: string): Promise<User> {
+    return this.users.find(user => user.email === email);
+  }
+}
