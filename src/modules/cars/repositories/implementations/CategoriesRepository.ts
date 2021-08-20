17e0efe2c60a0281425857cf41b1e5d024592a73
Matheus Felipe Zanin <matt_z6@hotmail.com>
2021-08-19 21:12:43 -0300

    refactor(cars): move entities to infra/typeorm folder

--- src/modules/cars/repositories/implementations/CategoriesRepository.ts
+++ src/modules/cars/repositories/implementations/CategoriesRepository.ts
@@ -1,6 +1,6 @@
 import { getRepository, Repository } from 'typeorm';
 
-import { Category } from '@modules/cars/entities/Category';
+import { Category } from '@modules/cars/infra/typeorm/entities/Category';
 
 import {
   CreateCategoryDTO,
--- src/modules/cars/repositories/implementations/SpecificationsRepository.ts
+++ src/modules/cars/repositories/implementations/SpecificationsRepository.ts
@@ -1,6 +1,6 @@
 import { getRepository, Repository } from 'typeorm';
 
-import { Specification } from '@modules/cars/entities/Specification';
+import { Specification } from '@modules/cars/infra/typeorm/entities/Specification';
 
 import {
   CreateSpecificationDTO,
