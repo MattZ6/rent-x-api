870ec961681200051baac5f60eb2c0d6e811c452
Matheus Felipe Zanin <matt_z6@hotmail.com>
2021-08-19 21:23:41 -0300

    chore(middlewares): move to shared/infra/http

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,10 +1,10 @@
 import { Router } from 'express';
 import multer from 'multer';
 
-import { ensureAuthenticated } from '@middlewares/ensure-authenticated';
-
 import { CreateUserController } from '@modules/users/usecases/create-user/CreateUserController';
 import { UpdateUserAvatarController } from '@modules/users/usecases/update-user-avatar/UpdateUserAvatarController';
+
+import { ensureAuthenticated } from '@shared/infra/http/middlewares/ensure-authenticated';
 
 import uploadConfig from '../config/upload';
 
