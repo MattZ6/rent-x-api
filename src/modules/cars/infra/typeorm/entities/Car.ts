d7959b1448e365cbd6f5f17ee0199371eb309e82
Matheus Felipe Zanin <matt_z6@hotmail.com>
2021-08-23 20:33:53 -0300

    chore(cars): add typeorm car entity

---
+++ src/modules/cars/infra/typeorm/entities/Car.ts
@@ -0,0 +1,58 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { v4 as generateUuid } from 'uuid';
+
+import { Category } from './Category';
+
+@Entity('cars')
+export class Car {
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  daily_rate: number;
+
+  @Column()
+  is_available: boolean;
+
+  @Column()
+  license_plate: string;
+
+  @Column()
+  fine_amount: number;
+
+  @Column()
+  brand: string;
+
+  @Column()
+  category_id: string;
+
+  @ManyToOne(() => Category)
+  @JoinColumn({ name: 'category_id' })
+  category: Category;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = generateUuid();
+    }
+  }
+}
--- src/modules/cars/infra/typeorm/entities/Category.ts
+++ src/modules/cars/infra/typeorm/entities/Category.ts
@@ -4,8 +4,11 @@ import {
   PrimaryColumn,
   CreateDateColumn,
   UpdateDateColumn,
+  OneToMany,
 } from 'typeorm';
 import { v4 as generateUuid } from 'uuid';
+
+import { Car } from './Car';
 
 @Entity('categories')
 export class Category {
@@ -23,6 +26,9 @@ export class Category {
 
   @UpdateDateColumn()
   updated_at: Date;
+
+  @OneToMany(() => Car, car => car.category_id)
+  cars: Car[];
 
   constructor() {
     if (!this.id) {
