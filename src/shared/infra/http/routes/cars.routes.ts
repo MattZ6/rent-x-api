aa8e7d8336012db14f2ee2b35c1d31d6a34403ef
Matheus Felipe Zanin <matt_z6@hotmail.com>
2021-08-25 19:45:21 -0300

    feat(ListAvailableCarsController): add controller, route and ensure it will be called without authentication

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 
 import { CreateCarController } from '@modules/cars/usecases/create-car/CreateCarController';
+import { ListAvailableCarsController } from '@modules/cars/usecases/list-available-cars-use-case/ListAvailableCarsController';
 
 import { ensureAuthenticated } from '@shared/infra/http/middlewares/ensure-authenticated';
 import { ensureIsAdminUser } from '@shared/infra/http/middlewares/ensure-is-admin-user';
@@ -8,6 +9,7 @@ import { ensureIsAdminUser } from '@shared/infra/http/middlewares/ensure-is-admi
 const routes = Router();
 
 const createCarController = new CreateCarController();
+const listAvailableCarsController = new ListAvailableCarsController();
 
 routes.post(
   '/',
@@ -15,5 +17,7 @@ routes.post(
   ensureIsAdminUser,
   createCarController.handle
 );
+
+routes.get('/available', listAvailableCarsController.handle);
 
 export default routes;
