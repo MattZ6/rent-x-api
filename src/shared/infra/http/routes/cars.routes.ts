b87c0c6f89e53a56484267ae379172b3a22452d9
Matheus Felipe Zanin <matt_z6@hotmail.com>
2021-08-25 21:48:41 -0300

    chore: add specifications to car controller

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 
+import { AddSpecificationsToCarController } from '@modules/cars/usecases/add-specifications-to-car/AddSpecificationsToCarController';
 import { CreateCarController } from '@modules/cars/usecases/create-car/CreateCarController';
 import { ListAvailableCarsController } from '@modules/cars/usecases/list-available-cars-use-case/ListAvailableCarsController';
 
@@ -10,6 +11,7 @@ const routes = Router();
 
 const createCarController = new CreateCarController();
 const listAvailableCarsController = new ListAvailableCarsController();
+const addSpecificationsToCarController = new AddSpecificationsToCarController();
 
 routes.post(
   '/',
@@ -19,5 +21,12 @@ routes.post(
 );
 
 routes.get('/available', listAvailableCarsController.handle);
+
+routes.post(
+  '/:id/specifications',
+  ensureAuthenticated,
+  ensureIsAdminUser,
+  addSpecificationsToCarController.handle
+);
 
 export default routes;
