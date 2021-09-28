33e73ed5ca10a12c62fee384b4701a1b7461c7a4
Matheus Felipe Zanin <matt_z6@hotmail.com>
2021-09-27 21:25:34 -0300

    chore: update database connection file

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -3,9 +3,12 @@ import { Connection, createConnection, getConnectionOptions } from 'typeorm';
 export default async (host = 'database_rentx'): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions();
 
+  const isTestEnv = process.env.NODE_ENV === 'test';
+
   return createConnection(
     Object.assign(defaultOptions, {
-      host,
+      host: isTestEnv ? 'localhost' : host,
+      database: isTestEnv ? 'rentx_test' : defaultOptions.database,
     })
   );
 };
