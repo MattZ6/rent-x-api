4dc2be4a1929b1dc89d7b20f70edf6cafae6a703
Matheus Felipe Zanin <matt_z6@hotmail.com>
2021-10-13 21:29:08 -0300

    chore(ICarsRepository): add availability update

--- src/modules/cars/repositories/ICarsRepository.ts
+++ src/modules/cars/repositories/ICarsRepository.ts
@@ -1,5 +1,6 @@
 import { ICreateCarDTO } from '../dtos/ICreateCarDTO';
 import { IGetAllAvailableDTO } from '../dtos/IGetAllAvailableDTO';
+import { IUpdateAvailabilityDTO } from '../dtos/IUpdateAvailabilityDTO';
 import { Car } from '../infra/typeorm/entities/Car';
 
 export interface ICarsRepository {
@@ -8,4 +9,5 @@ export interface ICarsRepository {
   findByLicensePlate(licensePlate: string): Promise<Car | undefined>;
   allAvailable(data: IGetAllAvailableDTO): Promise<Car[]>;
   findById(id: string): Promise<Car | undefined>;
+  updateAvailability(data: IUpdateAvailabilityDTO): Promise<void>;
 }
