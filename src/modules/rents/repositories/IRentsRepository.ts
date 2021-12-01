e1defb64e3cbc52498eb6a5917adb3aa4afabb5a
Matheus Felipe Zanin <matt_z6@hotmail.com>
2021-11-30 21:36:26 -0300

    feat(IRentsRepository): add findByUserId method

--- src/modules/rents/repositories/IRentsRepository.ts
+++ src/modules/rents/repositories/IRentsRepository.ts
@@ -3,6 +3,7 @@ import { Rent } from '../infra/typeorm/entities/Rent';
 
 export interface IRentsRepository {
   findById(id: string): Promise<Rent | undefined>;
+  findByUserId(user_id: string): Promise<Rent[]>;
   findOpenRentByCarId(car_id: string): Promise<Rent | undefined>;
   findOpenRentByUserId(user_id: string): Promise<Rent | undefined>;
   create(data: ICreateRentDTO): Promise<Rent>;
