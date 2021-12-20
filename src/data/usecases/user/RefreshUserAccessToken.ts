4ccb364399e472dd816e917caa5543943e52138b
Matheus Felipe Zanin <matt_z6@hotmail.com>
2021-12-20 17:45:46 -0300

    feat(data): add refresh user access token use case implementation

---
+++ src/data/usecases/user/RefreshUserAccessToken.ts
@@ -0,0 +1,71 @@
+import {
+  TokenExpiredError,
+  TokenNotFoundWithThisTokenFromUserError,
+} from '@domain/errors';
+import { IRefreshUserAccessTokenUseCase } from '@domain/usecases/user/RefreshUserAccessToken';
+
+import { IEncryptProvider } from '@data/protocols/providers/cryptography/cryptography';
+import { IGenerateUuidProvider } from '@data/protocols/providers/uuid';
+import {
+  ICreateUserTokenRepository,
+  IDeleteUserTokenByIdRepository,
+  IFindUserTokenByTokenFromUserRepository,
+} from '@data/protocols/repositories/user-token';
+
+export class RefreshUserAccessTokenUseCase
+  implements IRefreshUserAccessTokenUseCase
+{
+  constructor(
+    private readonly findUserTokenByTokenFromUser: IFindUserTokenByTokenFromUserRepository,
+    private readonly encryptProvider: IEncryptProvider,
+    private readonly generateUuidProvider: IGenerateUuidProvider,
+    private readonly refreshTokenExpiresTimeInMillisseconds: number,
+    private readonly createUserTokenRepository: ICreateUserTokenRepository,
+    private readonly deleteUserTokenByIdRepository: IDeleteUserTokenByIdRepository
+  ) {}
+
+  async execute(
+    data: IRefreshUserAccessTokenUseCase.Data
+  ): Promise<IRefreshUserAccessTokenUseCase.Response> {
+    const { refresh_token, user_id } = data;
+
+    const userToken =
+      await this.findUserTokenByTokenFromUser.findByTokenFromUser({
+        token: refresh_token,
+        user_id,
+      });
+
+    if (!userToken) {
+      return new TokenNotFoundWithThisTokenFromUserError();
+    }
+
+    const hasExpired = userToken.expires_in.getTime() < Date.now();
+
+    if (hasExpired) {
+      return new TokenExpiredError();
+    }
+
+    const accessToken = await this.encryptProvider.encrypt({
+      value: user_id,
+    });
+
+    const refreshToken = await this.generateUuidProvider.generate();
+
+    const expiresIn = new Date(
+      Date.now() + this.refreshTokenExpiresTimeInMillisseconds
+    );
+
+    await this.createUserTokenRepository.create({
+      token: refreshToken,
+      user_id,
+      expires_in: expiresIn,
+    });
+
+    await this.deleteUserTokenByIdRepository.deleteById(userToken.id);
+
+    return {
+      access_token: accessToken,
+      refresh_token: refreshToken,
+    };
+  }
+}
