b5803533b92c2e660febb1a2786ab8f55c635ccc
Matheus Felipe Zanin <matt_z6@hotmail.com>
2021-12-21 15:50:14 -0300

    feat(UpdateCarSpecificationUseCase): add implementation

---
+++ src/data/usecases/car/UpdateCarSpecification.ts
@@ -0,0 +1,53 @@
+import {
+  CarSpecificationNotFoundWithThisIdError,
+  CarSpecificationAlreadyExistsWithThisNameError,
+} from '@domain/errors';
+import { IUpdateCarSpecificationUseCase } from '@domain/usecases/car/UpdateCarSpecification';
+
+import {
+  ICheckIfCarSpecificationExistsByNameRepository,
+  IFindCarSpecificationByIdRepository,
+  IUpdateCarSpecificationRepository,
+} from '@data/protocols/repositories/specification';
+
+export class UpdateCarSpecificationUseCase
+  implements IUpdateCarSpecificationUseCase
+{
+  constructor(
+    private readonly findCarSpecificationByIdRepository: IFindCarSpecificationByIdRepository,
+    private readonly checkIfCarSpecificationExistsByNameRepository: ICheckIfCarSpecificationExistsByNameRepository,
+    private readonly updateCarSpecificationRepository: IUpdateCarSpecificationRepository
+  ) {}
+
+  async execute(
+    data: IUpdateCarSpecificationUseCase.Input
+  ): Promise<IUpdateCarSpecificationUseCase.Output> {
+    const { id, name, description } = data;
+
+    const specification =
+      await this.findCarSpecificationByIdRepository.findById(id);
+
+    if (!specification) {
+      throw new CarSpecificationNotFoundWithThisIdError();
+    }
+
+    const areSameName =
+      name.toLowerCase().trim() === specification.name.toLowerCase().trim();
+
+    if (!areSameName) {
+      const alreadyInUse =
+        await this.checkIfCarSpecificationExistsByNameRepository.checkIfExistsByName(
+          name
+        );
+
+      if (alreadyInUse) {
+        throw new CarSpecificationAlreadyExistsWithThisNameError();
+      }
+    }
+
+    specification.name = name;
+    specification.description = description;
+
+    return this.updateCarSpecificationRepository.update(specification);
+  }
+}
