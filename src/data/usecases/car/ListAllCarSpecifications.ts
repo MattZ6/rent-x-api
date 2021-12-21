67b9f8e345f3deeb5c410aba7a4dbcaa96bdbe1e
Matheus Felipe Zanin <matt_z6@hotmail.com>
2021-12-21 17:47:15 -0300

    feat(ListAllCarSpecificationsUseCase): add implementation

---
+++ src/data/usecases/car/ListAllCarSpecifications.ts
@@ -0,0 +1,24 @@
+import { IListAllCarSpecificationsUseCase } from '@domain/usecases/car/ListAllCarSpecifications';
+
+import { IFindAllCarSpecificationsRepository } from '@data/protocols/repositories/specification';
+
+export class ListAllCarSpecificationsUseCase
+  implements IListAllCarSpecificationsUseCase
+{
+  constructor(
+    private readonly defaultLimit: number,
+    private readonly defaultPage: number,
+    private readonly findAllCarSpecificationsRepository: IFindAllCarSpecificationsRepository
+  ) {}
+
+  async execute(
+    data?: IListAllCarSpecificationsUseCase.Input
+  ): Promise<IListAllCarSpecificationsUseCase.Output> {
+    const { limit = this.defaultLimit, page = this.defaultPage } = data ?? {};
+
+    return this.findAllCarSpecificationsRepository.findAll({
+      take: limit,
+      skip: page * limit,
+    });
+  }
+}
