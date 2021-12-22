566ea2769c87cbc697b281258057c454cf55cd54
Matheus Felipe Zanin <matt_z6@hotmail.com>
2021-12-22 08:34:45 -0300

    feat(DeleteCarSpecificationUseCase): add implementation

---
+++ src/data/usecases/car/DeleteCarSpecification.ts
@@ -0,0 +1,33 @@
+import { CarSpecificationNotFoundWithThisIdError } from '@domain/errors';
+import { IDeleteCarSpecificationUseCase } from '@domain/usecases/car/DeleteCarSpecification';
+
+import {
+  ICheckIfCarSpecificationExistsByIdRepository,
+  IDeleteCarSpecificationByIdRepository,
+} from '@data/protocols/repositories/specification';
+
+export class DeleteCarSpecificationUseCase
+  implements IDeleteCarSpecificationUseCase
+{
+  constructor(
+    private readonly checkIfCarSpecificationExistsByIdRepository: ICheckIfCarSpecificationExistsByIdRepository,
+    private readonly deleteCarSpecificationByIdRepository: IDeleteCarSpecificationByIdRepository
+  ) {}
+
+  async execute(
+    data: IDeleteCarSpecificationUseCase.Input
+  ): Promise<IDeleteCarSpecificationUseCase.Output> {
+    const { id } = data;
+
+    const exists =
+      await this.checkIfCarSpecificationExistsByIdRepository.checkIfExistsById(
+        id
+      );
+
+    if (!exists) {
+      throw new CarSpecificationNotFoundWithThisIdError();
+    }
+
+    await this.deleteCarSpecificationByIdRepository.deleteById(id);
+  }
+}
