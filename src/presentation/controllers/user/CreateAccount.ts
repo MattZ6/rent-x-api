c3ce7b906682eaa1256e1b569d5ea7d091b5d3d7
Matheus Felipe Zanin <matt_z6@hotmail.com>
2021-12-23 14:49:08 -0300

    feat(CreateAccountController): add implementation

---
+++ src/presentation/controllers/user/CreateAccount.ts
@@ -0,0 +1,51 @@
+import { UserAlreadyExistsWithThisEmailError } from '@domain/errors';
+import { ICreateUserUseCase } from '@domain/usecases/user/CreateUser';
+
+import { conflict, created } from '@presentation/helpers/http/http';
+import {
+  IController,
+  IHttpRequest,
+  IHttpRespose,
+} from '@presentation/protocols';
+
+class CreateAccountController implements IController {
+  constructor(private readonly createUserUseCase: ICreateUserUseCase) {}
+
+  async handle(
+    request: CreateAccountController.Request
+  ): Promise<CreateAccountController.Response> {
+    try {
+      const { name, driver_license, email, password } = request.body;
+
+      const user = await this.createUserUseCase.execute({
+        name,
+        driver_license,
+        email,
+        password,
+      });
+
+      return created(user);
+    } catch (error) {
+      if (error instanceof UserAlreadyExistsWithThisEmailError) {
+        return conflict(error);
+      }
+
+      throw error;
+    }
+  }
+}
+
+type CreateAccountBodyRequest = {
+  name: string;
+  email: string;
+  password: string;
+  driver_license: string;
+};
+
+namespace CreateAccountController {
+  export type Request = IHttpRequest<CreateAccountBodyRequest>;
+
+  export type Response = IHttpRespose;
+}
+
+export { CreateAccountController };
