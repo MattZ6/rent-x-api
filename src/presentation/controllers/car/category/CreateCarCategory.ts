a14e2b413f10d2f42ed9766f9a598604fdbc7be6
Matheus Felipe Zanin <matt_z6@hotmail.com>
2021-12-26 10:29:51 -0300

    feat(CreateCarCategory): add controller implementation

---
+++ src/presentation/controllers/car/category/CreateCarCategory.ts
@@ -0,0 +1,49 @@
+import { CarCategoryAlreadyExistsWithThisNameError } from '@domain/errors';
+import { ICreateCarCategoryUseCase } from '@domain/usecases/car/category/CreateCarCategory';
+
+import { conflict, created } from '@presentation/helpers/http/http';
+import {
+  IController,
+  IHttpRequest,
+  IHttpRespose,
+} from '@presentation/protocols';
+
+class CreateCarCategoryController implements IController {
+  constructor(
+    private readonly createCarCategoryUseCase: ICreateCarCategoryUseCase
+  ) {}
+
+  async handle(
+    request: CreateCarCategoryController.Request
+  ): Promise<CreateCarCategoryController.Response> {
+    try {
+      const { name, description } = request.body;
+
+      await this.createCarCategoryUseCase.execute({
+        name,
+        description,
+      });
+
+      return created<void>();
+    } catch (error) {
+      if (error instanceof CarCategoryAlreadyExistsWithThisNameError) {
+        return conflict(error);
+      }
+
+      throw error;
+    }
+  }
+}
+
+namespace CreateCarCategoryController {
+  type CreateCarCategoryBodyRequest = {
+    name: string;
+    description: string;
+  };
+
+  export type Request = IHttpRequest<CreateCarCategoryBodyRequest>;
+
+  export type Response = IHttpRespose;
+}
+
+export { CreateCarCategoryController };
