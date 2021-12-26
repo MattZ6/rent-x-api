391c6856fdb8eaca4bcf7d73791b482257e1030a
Matheus Felipe Zanin <matt_z6@hotmail.com>
2021-12-26 10:50:00 -0300

    refactor(mocks): change use cases mock return values

--- __tests__/presentation/mocks/create-car-specification-usecase.mock.ts
+++ __tests__/presentation/mocks/create-car-specification-usecase.mock.ts
@@ -1,14 +1,13 @@
-import { ICarSpecification } from '@domain/models/CarSpecification';
 import { ICreateCarSpecificationUseCase } from '@domain/usecases/car/specification/CreateCarSpecification';
 
-import { carMock } from '../../domain/models/car.mock';
+import { carSpecificationMock } from '../../domain/models/car-specification.mock';
 
 export class CreateCarSpecificationUseCaseSpy
   implements ICreateCarSpecificationUseCase
 {
   async execute(
     _: ICreateCarSpecificationUseCase.Input
-  ): Promise<ICarSpecification> {
-    return carMock;
+  ): Promise<ICreateCarSpecificationUseCase.Output> {
+    return carSpecificationMock;
   }
 }
--- __tests__/presentation/mocks/update-car-specification-usecase.mock.ts
+++ __tests__/presentation/mocks/update-car-specification-usecase.mock.ts
@@ -1,14 +1,13 @@
-import { ICarSpecification } from '@domain/models/CarSpecification';
 import { IUpdateCarSpecificationUseCase } from '@domain/usecases/car/specification/UpdateCarSpecification';
 
-import { carMock } from '../../domain/models/car.mock';
+import { carSpecificationMock } from '../../domain/models/car-specification.mock';
 
 export class UpdateCarSpecificationUseCaseSpy
   implements IUpdateCarSpecificationUseCase
 {
   async execute(
     _: IUpdateCarSpecificationUseCase.Input
-  ): Promise<ICarSpecification> {
-    return carMock;
+  ): Promise<IUpdateCarSpecificationUseCase.Output> {
+    return carSpecificationMock;
   }
 }
