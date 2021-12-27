3a0be43b33fd6fb063c5b70f944325382041e2bd
Matheus Felipe Zanin <matt_z6@hotmail.com>
2021-12-27 14:25:42 -0300

    feat: add postgres users repository implementation

---
+++ src/infra/database/typeorm/repositories/postgres/PostgresUsersRepository.ts
@@ -0,0 +1,46 @@
+import { getRepository, Raw, Repository } from 'typeorm';
+
+import { IUser } from '@domain/models/User';
+
+import {
+  CreateUserRepositoryData,
+  ICheckIfUserExistsByEmailRepository,
+  ICreateUserRepository,
+} from '@data/protocols/repositories/user';
+
+import { User } from '../../entities/User';
+
+export class PostgresUsersRepository
+  implements ICheckIfUserExistsByEmailRepository, ICreateUserRepository
+{
+  private repository: Repository<User>;
+
+  constructor() {
+    this.repository = getRepository(User);
+  }
+
+  async checkIfExistsByEmail(email: string): Promise<boolean> {
+    const count = await this.repository.count({
+      where: {
+        email: Raw(field => `LOWER(${field}) = LOWER(:value)`, {
+          value: email,
+        }),
+      },
+    });
+
+    return count >= 1;
+  }
+
+  async create(data: CreateUserRepositoryData): Promise<IUser> {
+    const { name, driver_license, email, password_hash } = data;
+
+    const user = this.repository.create({
+      name,
+      driver_license,
+      email,
+      password_hash,
+    });
+
+    return this.repository.save(user);
+  }
+}
