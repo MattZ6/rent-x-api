224efe35ac1f10323cbbece029164844ffcbab0f
Matheus Felipe Zanin <matt_z6@hotmail.com>
2021-12-27 14:29:07 -0300

    chore: add adapter for express routes

---
+++ src/main/adapters/express/express-route-adapter.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from 'express';
+
+import { IController } from '@presentation/protocols/Controller';
+
+export const adaptRoute =
+  (controller: IController) =>
+  async (req: Request, res: Response): Promise<Response> => {
+    try {
+      const response = await controller.handle({
+        params: req.params,
+        query: req.query,
+        body: req.body,
+        user_id: req.user_id,
+      });
+
+      return res.status(response.statusCode).json(response.body);
+    } catch (error) {
+      console.log(error);
+
+      return res.status(500).json({ message: 'Internal server error' });
+    }
+  };
