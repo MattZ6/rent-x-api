c42cf0226a9c61be41a405a7f9dcb65932b2320d
Matheus Felipe Zanin <matt_z6@hotmail.com>
2021-12-28 09:29:58 -0300

    feat(BcryptjsHashProvider): add compare hash implementation

--- src/infra/providers/cryptography/hash/BcryptjsHashProvider.ts
+++ src/infra/providers/cryptography/hash/BcryptjsHashProvider.ts
@@ -1,11 +1,23 @@
-import { hash } from 'bcryptjs';
+import { compare, hash } from 'bcryptjs';
 
-import { IGenerateHashProvider } from '@data/protocols/providers/cryptography/hash';
+import {
+  CompareHashDTO,
+  ICompareHashProvider,
+  IGenerateHashProvider,
+} from '@data/protocols/providers/cryptography/hash';
 
-export class BcryptjsHashProvider implements IGenerateHashProvider {
+export class BcryptjsHashProvider
+  implements IGenerateHashProvider, ICompareHashProvider
+{
   constructor(private readonly salt: number) {}
 
   async hash(value: string): Promise<string> {
     return hash(value, this.salt);
+  }
+
+  async compare(data: CompareHashDTO): Promise<boolean> {
+    const { value, value_to_compare } = data;
+
+    return compare(value, value_to_compare);
   }
 }
