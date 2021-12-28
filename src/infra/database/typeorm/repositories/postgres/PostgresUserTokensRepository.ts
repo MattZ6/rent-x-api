5edae345aac8f504ce40f77ea7a4c15e3d60f73a
Matheus Felipe Zanin <matt_z6@hotmail.com>
2021-12-28 13:03:59 -0300

    feat(PostgresUserTokensRepository): add find by token implementation

--- src/infra/database/typeorm/repositories/postgres/PostgresUserTokensRepository.ts
+++ src/infra/database/typeorm/repositories/postgres/PostgresUserTokensRepository.ts
@@ -5,12 +5,13 @@ import { IUserToken } from '@domain/models/UserToken';
 import {
   CreateUserTokenDTO,
   ICreateUserTokenRepository,
+  IFindUserTokenByTokenRepository,
 } from '@data/protocols/repositories/user-token';
 
 import { UserToken } from '@infra/database/typeorm/entities/UserToken';
 
 export class PostgresUserTokensRepository
-  implements ICreateUserTokenRepository
+  implements ICreateUserTokenRepository, IFindUserTokenByTokenRepository
 {
   private repository: Repository<UserToken>;
 
@@ -24,5 +25,9 @@ export class PostgresUserTokensRepository
     const userToken = this.repository.create({ token, expires_in, user_id });
 
     return this.repository.save(userToken);
+  }
+
+  async findByToken(token: string): Promise<IUserToken | undefined> {
+    return this.repository.findOne({ token });
   }
 }
