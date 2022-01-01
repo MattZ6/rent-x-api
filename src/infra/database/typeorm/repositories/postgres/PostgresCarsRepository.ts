28d4f95ce06459676fe5a8ba609b2f1c2d573949
Matheus Felipe Zanin <matt_z6@hotmail.com>
2022-01-01 14:22:37 -0300

    feat(PostgresCarsRepository): add create implementation

--- src/infra/database/typeorm/repositories/postgres/PostgresCarsRepository.ts
+++ src/infra/database/typeorm/repositories/postgres/PostgresCarsRepository.ts
@@ -1,11 +1,17 @@
 import { getRepository, Raw, Repository } from 'typeorm';
 
-import { ICheckIfCarExistsByLicensePlateRepository } from '@data/protocols/repositories/car';
+import { ICar } from '@domain/models/Car';
 
-import { Car } from '@modules/cars/infra/typeorm/entities/Car';
+import {
+  CreateCarDTO,
+  ICheckIfCarExistsByLicensePlateRepository,
+  ICreateCarRepository,
+} from '@data/protocols/repositories/car';
+
+import { Car } from '../../entities/Car';
 
 export class PostgresCarsRepository
-  implements ICheckIfCarExistsByLicensePlateRepository
+  implements ICheckIfCarExistsByLicensePlateRepository, ICreateCarRepository
 {
   private readonly repository: Repository<Car>;
 
@@ -23,5 +29,31 @@ export class PostgresCarsRepository
     });
 
     return count >= 1;
+  }
+
+  async create(data: CreateCarDTO): Promise<ICar> {
+    const {
+      name,
+      description,
+      license_plate,
+      daily_rate,
+      fine_amount,
+      brand_id,
+      category_id,
+      specifications,
+    } = data;
+
+    const car = this.repository.create({
+      name,
+      description,
+      license_plate,
+      daily_rate,
+      fine_amount,
+      brand_id,
+      category_id,
+      specifications,
+    });
+
+    return this.repository.save(car);
   }
 }
