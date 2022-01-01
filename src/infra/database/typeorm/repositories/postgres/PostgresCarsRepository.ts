abb5665e996841b76684c75bb245de2df1b328d1
Matheus Felipe Zanin <matt_z6@hotmail.com>
2022-01-01 14:01:08 -0300

    feat(PostgresCarsRepository): add check if exists by license plate implementation

--- src/infra/database/typeorm/repositories/postgres/PostgresCarsRepository.ts
+++ src/infra/database/typeorm/repositories/postgres/PostgresCarsRepository.ts
@@ -1,11 +1,27 @@
-import { getRepository, Repository } from 'typeorm';
+import { getRepository, Raw, Repository } from 'typeorm';
+
+import { ICheckIfCarExistsByLicensePlateRepository } from '@data/protocols/repositories/car';
 
 import { Car } from '@modules/cars/infra/typeorm/entities/Car';
 
-export class PostgresCarsRepository {
+export class PostgresCarsRepository
+  implements ICheckIfCarExistsByLicensePlateRepository
+{
   private readonly repository: Repository<Car>;
 
   constructor() {
     this.repository = getRepository(Car);
+  }
+
+  async checkIfExistsByLicensePlate(license_plate: string): Promise<boolean> {
+    const count = await this.repository.count({
+      where: {
+        license_plate: Raw(field => `LOWER(${field}) = LOWER(:value)`, {
+          value: license_plate,
+        }),
+      },
+    });
+
+    return count >= 1;
   }
 }
