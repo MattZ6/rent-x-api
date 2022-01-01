8d241b77c71f9d54fdefc01b445aed7abdf72b43
Matheus Felipe Zanin <matt_z6@hotmail.com>
2022-01-01 12:23:38 -0300

    feat(PostgresCarCategoriesRepository): add check if exists by name implementation

--- src/infra/database/typeorm/repositories/postgres/PostgresCarCategoriesRepository.ts
+++ src/infra/database/typeorm/repositories/postgres/PostgresCarCategoriesRepository.ts
@@ -1,11 +1,27 @@
-import { getRepository, Repository } from 'typeorm';
+import { getRepository, Raw, Repository } from 'typeorm';
+
+import { ICheckIfCarCategoryExistsByNameRepository } from '@data/protocols/repositories/car-category';
 
 import { CarCategory } from '../../entities/CarCategory';
 
-export class PostgresCarCategoriesRepository {
+export class PostgresCarCategoriesRepository
+  implements ICheckIfCarCategoryExistsByNameRepository
+{
   private readonly repository: Repository<CarCategory>;
 
   constructor() {
     this.repository = getRepository(CarCategory);
+  }
+
+  async checkIfExistsByName(name: string): Promise<boolean> {
+    const count = await this.repository.count({
+      where: {
+        name: Raw(field => `LOWER(${field}) = LOWER(:value)`, {
+          value: name,
+        }),
+      },
+    });
+
+    return count >= 1;
   }
 }
