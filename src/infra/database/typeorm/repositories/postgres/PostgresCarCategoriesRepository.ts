829b57bdfd67bd54555edd5cc9f2b68b3567442a
Matheus Felipe Zanin <matt_z6@hotmail.com>
2022-01-01 12:24:50 -0300

    feat(PostgresCarCategoriesRepository): add create implementation

--- src/infra/database/typeorm/repositories/postgres/PostgresCarCategoriesRepository.ts
+++ src/infra/database/typeorm/repositories/postgres/PostgresCarCategoriesRepository.ts
@@ -1,11 +1,19 @@
 import { getRepository, Raw, Repository } from 'typeorm';
 
-import { ICheckIfCarCategoryExistsByNameRepository } from '@data/protocols/repositories/car-category';
+import { ICarCategory } from '@domain/models/CarCategory';
+
+import {
+  CreateCarCategoryDTO,
+  ICheckIfCarCategoryExistsByNameRepository,
+  ICreateCarCategoryRepository,
+} from '@data/protocols/repositories/car-category';
 
 import { CarCategory } from '../../entities/CarCategory';
 
 export class PostgresCarCategoriesRepository
-  implements ICheckIfCarCategoryExistsByNameRepository
+  implements
+    ICheckIfCarCategoryExistsByNameRepository,
+    ICreateCarCategoryRepository
 {
   private readonly repository: Repository<CarCategory>;
 
@@ -23,5 +31,13 @@ export class PostgresCarCategoriesRepository
     });
 
     return count >= 1;
+  }
+
+  async create(data: CreateCarCategoryDTO): Promise<ICarCategory> {
+    const { name, description } = data;
+
+    const carCategory = this.repository.create({ name, description });
+
+    return this.repository.save(carCategory);
   }
 }
