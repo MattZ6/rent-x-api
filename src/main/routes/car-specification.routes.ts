6ce629c372251351f7573d941d8263a17e308f50
Matheus Felipe Zanin <matt_z6@hotmail.com>
2022-01-01 11:47:41 -0300

    feat(UpdateCarSpecification): add route

--- src/main/routes/car-specification.routes.ts
+++ src/main/routes/car-specification.routes.ts
@@ -3,12 +3,19 @@ import { Router } from 'express';
 import { adaptRoute } from '@main/adapters/express/express-route-adapter';
 import { authenticationMiddleware } from '@main/config/middlewares/authentication';
 import { makeCreateCarSpecificationController } from '@main/factories/controllers/car/specification/CreateCarSpecificationControllerFactory';
+import { makeUpdateCarSpecificationController } from '@main/factories/controllers/car/specification/UpdateCarSpecificationControllerFactory';
 
 const carSpecificationsRoutes = Router();
+
 carSpecificationsRoutes.post(
   '/',
   authenticationMiddleware,
   adaptRoute(makeCreateCarSpecificationController())
+);
+carSpecificationsRoutes.put(
+  '/:id',
+  authenticationMiddleware,
+  adaptRoute(makeUpdateCarSpecificationController())
 );
 
 export default carSpecificationsRoutes;
