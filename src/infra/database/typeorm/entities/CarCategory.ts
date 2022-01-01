e7606db3c1579c6287e67b312df5133f611236f3
Matheus Felipe Zanin <matt_z6@hotmail.com>
2022-01-01 12:23:12 -0300

    feat(CarCategory): add database model

---
+++ src/infra/database/typeorm/entities/CarCategory.ts
@@ -0,0 +1,36 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { v4 } from 'uuid';
+
+import { ICarCategory } from '@domain/models/CarCategory';
+
+import { tableNames } from '../constants';
+
+@Entity(tableNames.CAR_CATEGORIES)
+export class CarCategory implements ICarCategory {
+  @PrimaryColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  description: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = v4();
+    }
+  }
+}
