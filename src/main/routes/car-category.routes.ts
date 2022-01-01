1ab3badd16e623eb84e442e693355bea9998c5c9
Matheus Felipe Zanin <matt_z6@hotmail.com>
2022-01-01 13:09:12 -0300

    feat(UpdateCarCategory): add factories and route

--- src/main/routes/car-category.routes.ts
+++ src/main/routes/car-category.routes.ts
@@ -3,6 +3,7 @@ import { Router } from 'express';
 import { adaptRoute } from '@main/adapters/express/express-route-adapter';
 import { authenticationMiddleware } from '@main/config/middlewares/authentication';
 import { makeCreateCarCategoryController } from '@main/factories/controllers/car/category/CreateCarCategoryControllerFactory';
+import { makeUpdateCarCategoryController } from '@main/factories/controllers/car/category/UpdateCarCategoryControllerFactory';
 
 const carCategoriesRoutes = Router();
 
@@ -10,6 +11,12 @@ carCategoriesRoutes.post(
   '/',
   authenticationMiddleware,
   adaptRoute(makeCreateCarCategoryController())
+);
+
+carCategoriesRoutes.put(
+  '/',
+  authenticationMiddleware,
+  adaptRoute(makeUpdateCarCategoryController())
 );
 
 export default carCategoriesRoutes;
