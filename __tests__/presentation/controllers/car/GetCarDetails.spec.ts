f0f0cc6f8a4f0d84774e0c96b4d8adb3d111536d
Matheus Felipe Zanin <matt_z6@hotmail.com>
2022-01-04 14:56:26 -0300

    test(GetCarDetails): add controller tests

---
+++ __tests__/presentation/controllers/car/GetCarDetails.spec.ts
@@ -0,0 +1,76 @@
+import faker from 'faker';
+
+import { CarNotFoundWithThisIdError } from '@domain/errors';
+
+import { GetCarDetailsController } from '@presentation/controllers/car/GetCarDetails';
+import { notFound, ok } from '@presentation/helpers/http/http';
+
+import { carMock } from '../../../domain/models/car.mock';
+import { GetCarDetailsUseCaseSpy } from '../../mocks';
+
+let getCarDetailsUseCaseSpy: GetCarDetailsUseCaseSpy;
+
+let getCarDetailsController: GetCarDetailsController;
+
+const getCarDetailsControllerRequestMock: GetCarDetailsController.Request = {
+  params: {
+    id: faker.datatype.uuid(),
+  },
+};
+
+describe('GetCarDetailsController', () => {
+  beforeEach(() => {
+    getCarDetailsUseCaseSpy = new GetCarDetailsUseCaseSpy();
+
+    getCarDetailsController = new GetCarDetailsController(
+      getCarDetailsUseCaseSpy
+    );
+  });
+
+  it('should call GetCarDetailsUseCase once with correct values', async () => {
+    const executeSpy = jest.spyOn(getCarDetailsUseCaseSpy, 'execute');
+
+    await getCarDetailsController.handle(getCarDetailsControllerRequestMock);
+
+    expect(executeSpy).toHaveBeenCalledTimes(1);
+    expect(executeSpy).toHaveBeenLastCalledWith({
+      car_id: getCarDetailsControllerRequestMock.params.id,
+    });
+  });
+
+  it('should throw if GetCarDetailsUseCase throws', async () => {
+    jest
+      .spyOn(getCarDetailsUseCaseSpy, 'execute')
+      .mockRejectedValueOnce(new Error());
+
+    const promise = getCarDetailsController.handle(
+      getCarDetailsControllerRequestMock
+    );
+
+    await expect(promise).rejects.toThrow();
+  });
+
+  it('should return not found (404) if GetCarDetailsUseCase throws CarNotFoundWithThisIdError', async () => {
+    const error = new CarNotFoundWithThisIdError();
+
+    jest.spyOn(getCarDetailsUseCaseSpy, 'execute').mockRejectedValueOnce(error);
+
+    const result = await getCarDetailsController.handle(
+      getCarDetailsControllerRequestMock
+    );
+
+    expect(result).toEqual(notFound(error));
+  });
+
+  it('should return ok (200) on success', async () => {
+    jest
+      .spyOn(getCarDetailsUseCaseSpy, 'execute')
+      .mockResolvedValueOnce(carMock);
+
+    const result = await getCarDetailsController.handle(
+      getCarDetailsControllerRequestMock
+    );
+
+    expect(result).toEqual(ok(carMock));
+  });
+});
--- __tests__/presentation/mocks/index.ts
+++ __tests__/presentation/mocks/index.ts
@@ -6,6 +6,7 @@ export * from './create-car-usecase.mock';
 export * from './create-user-usecase.mock';
 export * from './delete-car-specification-usecase.mock';
 export * from './get-user-profile-usecase.mock';
+export * from './get-car-details-usecase.mock';
 export * from './list-all-car-brands-usecase.mock';
 export * from './list-all-car-categories-usecase.mock';
 export * from './list-all-car-specifications-usecase.mock';
