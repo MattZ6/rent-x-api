de48b367e3a2aae1a1013b54d4014ca274bcb78b
Matheus Felipe Zanin <matt_z6@hotmail.com>
2022-01-04 14:58:20 -0300

    feat(GetCarDetails): add route

--- src/main/routes/car.routes.ts
+++ src/main/routes/car.routes.ts
@@ -3,6 +3,7 @@ import { Router } from 'express';
 import { adaptRoute } from '@main/adapters/express/express-route-adapter';
 import { authenticationMiddleware } from '@main/config/middlewares/authentication';
 import { makeCreateCarController } from '@main/factories/controllers/car/CreateCarControllerFactory';
+import { makeGetCarDetailsController } from '@main/factories/controllers/car/GetCarDetailsControllerFactory';
 import { makeListCarsController } from '@main/factories/controllers/car/ListCarsControllerFactory';
 
 const carsRoutes = Router();
@@ -17,6 +18,12 @@ carsRoutes.get(
   '/',
   authenticationMiddleware,
   adaptRoute(makeListCarsController())
+);
+
+carsRoutes.get(
+  '/:id',
+  authenticationMiddleware,
+  adaptRoute(makeGetCarDetailsController())
 );
 
 export default carsRoutes;
