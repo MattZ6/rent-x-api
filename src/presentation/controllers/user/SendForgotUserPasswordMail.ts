85286353d0ed8091b7bd51469e00700eadded4fc
Matheus Felipe Zanin <matt_z6@hotmail.com>
2022-01-04 20:31:37 -0300

    feat(SendForgotPasswordEmail): add controller implementation

---
+++ src/presentation/controllers/user/SendForgotUserPasswordMail.ts
@@ -0,0 +1,51 @@
+import { UserNotFoundWithThisEmailError } from '@domain/errors';
+import { ISendForgotUserPasswordMailUseCase } from '@domain/usecases/user/SendForgotUserPasssordMail';
+
+import { noContent } from '@presentation/helpers/http/http';
+import {
+  IController,
+  IHttpRequest,
+  IHttpResponse,
+} from '@presentation/protocols';
+
+class SendForgotUserPasswordMailController implements IController {
+  constructor(
+    private readonly sendForgotUserPasswordMailUseCase: ISendForgotUserPasswordMailUseCase
+  ) {}
+
+  async handle(
+    request: SendForgotUserPasswordMailController.Request
+  ): Promise<SendForgotUserPasswordMailController.Response> {
+    try {
+      const { email } = request.body;
+
+      await this.sendForgotUserPasswordMailUseCase.execute({
+        email,
+      });
+
+      return noContent();
+    } catch (error) {
+      if (error instanceof UserNotFoundWithThisEmailError) {
+        return noContent();
+      }
+
+      throw error;
+    }
+  }
+}
+
+namespace SendForgotUserPasswordMailController {
+  type SendForgotUserPasswordMailBodyRequest = {
+    email: string;
+  };
+
+  export type Request = IHttpRequest<
+    SendForgotUserPasswordMailBodyRequest,
+    void,
+    void
+  >;
+
+  export type Response = IHttpResponse;
+}
+
+export { SendForgotUserPasswordMailController };
