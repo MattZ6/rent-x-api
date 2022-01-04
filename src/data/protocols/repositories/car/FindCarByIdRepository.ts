29da4ddb8287f4014f30809427a9571781096adb
Matheus Felipe Zanin <matt_z6@hotmail.com>
2022-01-04 14:44:51 -0300

    feat(data): add FindCarByIdRepository protocol

---
+++ src/data/protocols/repositories/car/FindCarByIdRepository.ts
@@ -0,0 +1,18 @@
+import { ICar } from '@domain/models/Car';
+
+interface IFindCarByIdRepository {
+  findById(
+    data: IFindCarByIdRepository.Input
+  ): Promise<IFindCarByIdRepository.Output>;
+}
+
+namespace IFindCarByIdRepository {
+  export type Input = {
+    id: string;
+    relations: Array<keyof Pick<ICar, 'brand' | 'category' | 'specifications'>>;
+  };
+
+  export type Output = undefined | ICar;
+}
+
+export { IFindCarByIdRepository };
--- src/data/protocols/repositories/car/index.ts
+++ src/data/protocols/repositories/car/index.ts
@@ -1,3 +1,4 @@
 export * from './CheckIfCarExistsByLicensePlateRepository';
 export * from './CreateCarRepository';
 export * from './FindAllCarsRepository';
+export * from './FindCarByIdRepository';
