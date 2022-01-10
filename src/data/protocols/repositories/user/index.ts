33dc98520d48a9594b3fee40bd533b2dcd6e9c22
Matheus Felipe Zanin <matt_z6@hotmail.com>
2022-01-10 20:45:12 -0300

    feat(UsersRepository): add check if exists by id protocol

--- src/data/protocols/repositories/user/index.ts
+++ src/data/protocols/repositories/user/index.ts
@@ -1,5 +1,6 @@
 export * from './CheckIfUserExistsByEmailRepository';
 export * from './CheckIfUserExistsByDriverLicenseRepository';
+export * from './CheckIfUserExistsByIdRepository';
 export * from './CreateUserRepository';
 export * from './FindUserByEmailRepository';
 export * from './FindUserByIdRepository';
