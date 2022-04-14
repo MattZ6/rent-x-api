8a8b6190c97ddc0da0c6bdca1ed6b6661645784b
Matheus Felipe Zanin <matt_z6@hotmail.com>
2022-04-14 15:42:48 -0300

    refactor(domain): user error exports

--- src/domain/errors/user/index.ts
+++ src/domain/errors/user/index.ts
@@ -1,7 +1,7 @@
-export * from './WrongPassword';
 export * from './AlreadyExistsWithProvidedDriverLicense';
 export * from './AlreadyExistsWithProvidedEmail';
 export * from './NotFoundWithProvidedEmail';
 export * from './NotFoundWithProvidedId';
+export * from './WrongPassword';
 
 export * from './token';
