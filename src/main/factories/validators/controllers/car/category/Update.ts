f5cda0b696290aa314f0e2f38718a432e3771d58
Matheus Felipe Zanin <matt_z6@hotmail.com>
2022-04-26 15:27:56 -0300

    feat(UpdateCarCategoryController): add validation

---
+++ src/main/factories/validators/controllers/car/category/Update.ts
@@ -0,0 +1,19 @@
+import { UpdateCarCategoryController } from '@presentation/controllers/car/category/Update';
+import {
+  MinLengthFieldValidation,
+  RequiredFieldValidation,
+  UuidFieldValidation,
+  ValidationComposite,
+} from '@presentation/validations/validators';
+
+export function makeUpdateCarCategoryControllerValidation(): ValidationComposite {
+  type Input = UpdateCarCategoryController.RequestBody &
+    UpdateCarCategoryController.RequestParams;
+
+  return new ValidationComposite<Input>([
+    new RequiredFieldValidation('id'),
+    new UuidFieldValidation('id'),
+    new RequiredFieldValidation('name'),
+    new MinLengthFieldValidation('name', 3, true),
+  ]);
+}
