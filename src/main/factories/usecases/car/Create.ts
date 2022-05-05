4f8ae45defb6ea0617b217544c1d37ad9d1f0bfc
Matheus Felipe Zanin <matt_z6@hotmail.com>
2022-05-05 16:44:11 -0300

    feat: add prisma car categories repository

--- src/main/factories/usecases/car/Create.ts
+++ src/main/factories/usecases/car/Create.ts
@@ -2,13 +2,13 @@ import { CreateCarUseCase } from '@application/usecases/car/Create';
 
 import { makePostgresCarsRepository } from '@main/factories/repositories/Car';
 import { makeCarBrandsRepository } from '@main/factories/repositories/CarBrand';
-import { makePostgresCarCategoriesRepository } from '@main/factories/repositories/CarCategory';
+import { makeCarCategoriesRepository } from '@main/factories/repositories/CarCategory';
 import { makePostgresCarSpecificationsRepository } from '@main/factories/repositories/CarSpecification';
 
 export function makeCreateCarUseCase() {
   const carsRepository = makePostgresCarsRepository();
   const carBrandsRepository = makeCarBrandsRepository();
-  const carCategoriesRepository = makePostgresCarCategoriesRepository();
+  const carCategoriesRepository = makeCarCategoriesRepository();
   const carSpecificationsRepository = makePostgresCarSpecificationsRepository();
 
   return new CreateCarUseCase(
--- src/main/factories/usecases/car/category/ListAll.ts
+++ src/main/factories/usecases/car/category/ListAll.ts
@@ -1,16 +1,16 @@
 import { ListAllCarCategoriesUseCase } from '@application/usecases/car/category/ListAll';
 
 import { carCategoryConfig } from '@main/config/environment/carCategory';
-import { makePostgresCarCategoriesRepository } from '@main/factories/repositories/CarCategory';
+import { makeCarCategoriesRepository } from '@main/factories/repositories/CarCategory';
 
 export function makeListAllCarCategoriesUseCase() {
-  const postgresCarCategoriesRepository = makePostgresCarCategoriesRepository();
+  const carCategoriesRepository = makeCarCategoriesRepository();
 
   return new ListAllCarCategoriesUseCase(
     carCategoryConfig.DEFAULT_SORT_BY,
     carCategoryConfig.DEFAULT_ORDER_BY,
     carCategoryConfig.DEFAULT_LIMIT,
     carCategoryConfig.DEFAULT_OFFSET,
-    postgresCarCategoriesRepository
+    carCategoriesRepository
   );
 }
