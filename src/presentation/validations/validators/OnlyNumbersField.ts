74e7f9e9e5047b86282e26d29e22f0ff70e8624a
Matheus Felipe Zanin <matt_z6@hotmail.com>
2022-05-11 20:08:06 -0300

    refactor(OnlyNumbersFieldValidation): change implementation

--- src/presentation/validations/validators/OnlyNumbersField.ts
+++ src/presentation/validations/validators/OnlyNumbersField.ts
@@ -2,23 +2,24 @@ import { OnlyNumbersFieldError } from '@presentation/errors';
 import { IValidation } from '@presentation/protocols';
 
 export class OnlyNumbersFieldValidation<I = unknown> implements IValidation<I> {
+  private readonly ONLY_NUMBERS_REGEX = /^[0-9]+$/;
+  private readonly ONLY_NUMBERS_WITH_DOT_REGEX = /^[+-]?([0-9]*[.])?[0-9]+$/;
+
   constructor(
     private readonly fieldName: keyof I,
     private readonly mustConsiderDot = false
   ) {}
 
   validate(input: I) {
-    const value = String(input[this.fieldName] ?? '').trim();
+    const value = String(input[this.fieldName]);
 
-    if (!value.length) {
-      return null;
+    let isValid = this.ONLY_NUMBERS_REGEX.test(value);
+
+    if (this.mustConsiderDot) {
+      isValid = this.ONLY_NUMBERS_WITH_DOT_REGEX.test(value);
     }
 
-    const onlyNumbersRegex = this.mustConsiderDot
-      ? /^[+-]?([0-9]*[.])?[0-9]+$/
-      : /^[0-9]+$/;
-
-    if (!onlyNumbersRegex.test(value)) {
+    if (!isValid) {
       return new OnlyNumbersFieldError(String(this.fieldName));
     }
 
