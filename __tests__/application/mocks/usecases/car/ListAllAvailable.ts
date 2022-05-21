08196d1865f24902d0e700e564ef66d1f091be3c
Matheus Felipe Zanin <matt_z6@hotmail.com>
2022-05-20 22:47:34 -0300

    test(ListAllAvailableCarsUseCase): add tests

---
+++ __tests__/application/mocks/usecases/car/ListAllAvailable.ts
@@ -0,0 +1,87 @@
+import { faker } from '@faker-js/faker';
+
+import {
+  CarTransmissionTypeEnum,
+  CarTypeOfFuelEnum,
+} from '@domain/entities/Car';
+import { IListAllAvailableCarsUseCase } from '@domain/usecases/car/ListAllAvailable';
+
+export function makeListAllAvailableCarsUseCaseDefaultSortByMock(): IListAllAvailableCarsUseCase.SortBy {
+  return faker.helpers.arrayElement<IListAllAvailableCarsUseCase.SortBy>([
+    'name',
+    'created_at',
+    'horse_power',
+    'max_speed',
+    'number_of_seats',
+  ]);
+}
+
+export function makeListAllAvailableCarsUseCaseDefaultOrderByMock(): IListAllAvailableCarsUseCase.OrderBy {
+  return faker.helpers.arrayElement<IListAllAvailableCarsUseCase.OrderBy>([
+    'asc',
+    'desc',
+  ]);
+}
+
+export function makeListAllAvailableCarsUseCaseDefaultLimitMock(): IListAllAvailableCarsUseCase.Limit {
+  return faker.datatype.number({ min: 1 });
+}
+
+export function makeListAllAvailableCarsUseCaseDefaultOffsetMock(): IListAllAvailableCarsUseCase.Offset {
+  return faker.datatype.number({ min: 0 });
+}
+
+export function makeListAllAvailableCarsUseCaseInputMock(): IListAllAvailableCarsUseCase.Input {
+  const startDate = faker.datatype.datetime();
+
+  return {
+    brand_id: faker.helpers.arrayElement([
+      undefined,
+      null,
+      faker.datatype.uuid(),
+    ]),
+    category_id: faker.helpers.arrayElement([
+      undefined,
+      null,
+      faker.datatype.uuid(),
+    ]),
+    type_of_fuel: faker.helpers.arrayElement<CarTypeOfFuelEnum>([
+      undefined,
+      null,
+      'ALCOHOL',
+      'ELETRICITY',
+      'GAS',
+    ]),
+    transmission_type: faker.helpers.arrayElement<CarTransmissionTypeEnum>([
+      undefined,
+      null,
+      'AUTO',
+      'MANUAL',
+    ]),
+    min_daily_rate: faker.helpers.arrayElement([
+      undefined,
+      null,
+      faker.datatype.number(),
+    ]),
+    max_daily_rate: faker.helpers.arrayElement([
+      undefined,
+      null,
+      faker.datatype.number(),
+    ]),
+    start_date: faker.helpers.arrayElement([undefined, null, startDate]),
+    end_date: faker.helpers.arrayElement([
+      undefined,
+      null,
+      faker.date.soon(faker.datatype.number({ min: 1, max: 30 }), startDate),
+    ]),
+    search: faker.helpers.arrayElement([
+      undefined,
+      null,
+      faker.datatype.string(),
+    ]),
+    sort_by: makeListAllAvailableCarsUseCaseDefaultSortByMock(),
+    order_by: makeListAllAvailableCarsUseCaseDefaultOrderByMock(),
+    limit: makeListAllAvailableCarsUseCaseDefaultLimitMock(),
+    offset: makeListAllAvailableCarsUseCaseDefaultOffsetMock(),
+  };
+}
---
+++ __tests__/application/usecases/car/ListAllAvailable.spec.ts
@@ -0,0 +1,118 @@
+import { IListAllAvailableCarsUseCase } from '@domain/usecases/car/ListAllAvailable';
+
+import { ListAllAvailableCarsUseCase } from '@application/usecases/car/ListAllAvailable';
+
+import { makeCarMock, makeErrorMock } from '../../../domain';
+import {
+  FindAllCarsRepositorySpy,
+  makeListAllAvailableCarsUseCaseDefaultLimitMock,
+  makeListAllAvailableCarsUseCaseDefaultOffsetMock,
+  makeListAllAvailableCarsUseCaseDefaultOrderByMock,
+  makeListAllAvailableCarsUseCaseDefaultSortByMock,
+  makeListAllAvailableCarsUseCaseInputMock,
+} from '../../mocks';
+
+let listAllAvailableCarsUseCaseDefaultSortByMock: IListAllAvailableCarsUseCase.SortBy;
+let listAllAvailableCarsUseCaseDefaultOrderByMock: IListAllAvailableCarsUseCase.OrderBy;
+let listAllAvailableCarsUseCaseDefaultLimitMock: IListAllAvailableCarsUseCase.Limit;
+let listAllAvailableCarsUseCaseDefaultOffsetMock: IListAllAvailableCarsUseCase.Offset;
+let findAllCarsRepositorySpy: FindAllCarsRepositorySpy;
+
+let listAllAvailableCarsUseCase: ListAllAvailableCarsUseCase;
+
+describe('ListAllAvailableCarsUseCase', () => {
+  beforeEach(() => {
+    listAllAvailableCarsUseCaseDefaultSortByMock =
+      makeListAllAvailableCarsUseCaseDefaultSortByMock();
+    listAllAvailableCarsUseCaseDefaultOrderByMock =
+      makeListAllAvailableCarsUseCaseDefaultOrderByMock();
+    listAllAvailableCarsUseCaseDefaultLimitMock =
+      makeListAllAvailableCarsUseCaseDefaultLimitMock();
+    listAllAvailableCarsUseCaseDefaultOffsetMock =
+      makeListAllAvailableCarsUseCaseDefaultOffsetMock();
+    findAllCarsRepositorySpy = new FindAllCarsRepositorySpy();
+
+    listAllAvailableCarsUseCase = new ListAllAvailableCarsUseCase(
+      listAllAvailableCarsUseCaseDefaultSortByMock,
+      listAllAvailableCarsUseCaseDefaultOrderByMock,
+      listAllAvailableCarsUseCaseDefaultLimitMock,
+      listAllAvailableCarsUseCaseDefaultOffsetMock,
+      findAllCarsRepositorySpy
+    );
+  });
+
+  it('should call FindAllCarsRepository once with correct values', async () => {
+    const findAllSpy = jest.spyOn(findAllCarsRepositorySpy, 'findAll');
+
+    const input = makeListAllAvailableCarsUseCaseInputMock();
+
+    await listAllAvailableCarsUseCase.execute(input);
+
+    expect(findAllSpy).toHaveBeenCalledTimes(1);
+    expect(findAllSpy).toHaveBeenCalledWith({
+      sort_by: input.sort_by,
+      order_by: input.order_by,
+      take: input.limit,
+      skip: input.offset,
+      brand_id: input.brand_id,
+      category_id: input.category_id,
+      transmission_type: input.transmission_type,
+      type_of_fuel: input.type_of_fuel,
+      min_daily_rate: input.min_daily_rate,
+      max_daily_rate: input.max_daily_rate,
+      start_date: input.start_date,
+      end_date: input.end_date,
+      search: input.search,
+      include: {
+        brand: true,
+        category: true,
+      },
+    });
+  });
+
+  it('should call FindAllCarsRepository with default values if no input', async () => {
+    const findAllSpy = jest.spyOn(findAllCarsRepositorySpy, 'findAll');
+
+    await listAllAvailableCarsUseCase.execute({});
+
+    expect(findAllSpy).toHaveBeenCalledTimes(1);
+    expect(findAllSpy).toHaveBeenCalledWith({
+      sort_by: listAllAvailableCarsUseCaseDefaultSortByMock,
+      order_by: listAllAvailableCarsUseCaseDefaultOrderByMock,
+      take: listAllAvailableCarsUseCaseDefaultLimitMock,
+      skip: listAllAvailableCarsUseCaseDefaultOffsetMock,
+      include: {
+        brand: true,
+        category: true,
+      },
+    });
+  });
+
+  it('should throw if FindAllCarsRepository throws', async () => {
+    const errorMock = makeErrorMock();
+
+    jest
+      .spyOn(findAllCarsRepositorySpy, 'findAll')
+      .mockRejectedValueOnce(errorMock);
+
+    const input = makeListAllAvailableCarsUseCaseInputMock();
+
+    const promise = listAllAvailableCarsUseCase.execute(input);
+
+    await expect(promise).rejects.toThrowError(errorMock);
+  });
+
+  it('should return Cars on success', async () => {
+    const carsMock = [makeCarMock(), makeCarMock(), makeCarMock()];
+
+    jest
+      .spyOn(findAllCarsRepositorySpy, 'findAll')
+      .mockResolvedValueOnce(carsMock);
+
+    const input = makeListAllAvailableCarsUseCaseInputMock();
+
+    const output = await listAllAvailableCarsUseCase.execute(input);
+
+    expect(output).toEqual(carsMock);
+  });
+});
