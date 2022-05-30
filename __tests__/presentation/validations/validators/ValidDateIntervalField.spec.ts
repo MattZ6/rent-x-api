061f685fcb6291e4eff9dad034084a33f590912b
Matheus Felipe Zanin <matt_z6@hotmail.com>
2022-05-30 17:26:46 -0300

    fix(faker-js): remove named imports

--- __tests__/presentation/validations/validators/ValidDateIntervalField.spec.ts
+++ __tests__/presentation/validations/validators/ValidDateIntervalField.spec.ts
@@ -1,4 +1,4 @@
-import faker from '@faker-js/faker';
+import { faker } from '@faker-js/faker';
 
 import { InvalidDateIntervalFieldError } from '@presentation/errors';
 import { ValidDateIntervalFieldValidation } from '@presentation/validations/validators';
