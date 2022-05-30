5795a378a71189eb140732e2dfbe76ccfeb87b18
Matheus Felipe Zanin <matt_z6@hotmail.com>
2022-05-30 17:23:42 -0300

    fix(faker-js): remove named imports

--- __tests__/presentation/validations/validators/Composite.spec.ts
+++ __tests__/presentation/validations/validators/Composite.spec.ts
@@ -1,4 +1,4 @@
-import faker from '@faker-js/faker';
+import { faker } from '@faker-js/faker';
 
 import { MinLengthFieldError, RequiredFieldError } from '@presentation/errors';
 import { ValidationComposite } from '@presentation/validations/validators';
