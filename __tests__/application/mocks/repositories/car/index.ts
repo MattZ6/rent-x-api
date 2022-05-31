909896fe386d4bf89c01fb8f9d15e59177215f83
Matheus Felipe Zanin <matt_z6@hotmail.com>
2022-05-31 12:00:39 -0300

    test(AddImagesToCarUseCase): add tests

--- __tests__/application/mocks/repositories/car/index.ts
+++ __tests__/application/mocks/repositories/car/index.ts
@@ -1,4 +1,5 @@
 export * from './car-brands.mock';
 export * from './car-categories.mock';
+export * from './car-images.mock';
 export * from './car-specifications.mock';
 export * from './cars.mock';
