2b4806b789457b68956ac45b12cb0e3f6a84449a
Matheus Felipe Zanin <matt_z6@hotmail.com>
2022-05-31 12:00:53 -0300

    feat(AddImagesToCarController): add implementation

---
+++ src/presentation/controllers/car/images/Add.ts
@@ -0,0 +1,68 @@
+import { CarNotFoundWithProvidedIdError } from '@domain/errors';
+import { IAddImagesToCarUseCase } from '@domain/usecases/car/image/Add';
+
+import { ValidationError } from '@presentation/errors';
+import { badRequest, noContent, notFound } from '@presentation/helpers/http';
+import {
+  IController,
+  IHttpRequest,
+  IHttpResponse,
+  IValidation,
+} from '@presentation/protocols';
+
+class AddImagesToCarController implements IController {
+  constructor(
+    private readonly validation: IValidation,
+    private readonly addImagesToCarUseCase: IAddImagesToCarUseCase
+  ) {}
+
+  async handle(
+    request: AddImagesToCarController.Request
+  ): Promise<AddImagesToCarController.Response> {
+    try {
+      const validationError = this.validation.validate(request);
+
+      if (validationError) {
+        throw validationError;
+      }
+
+      const { car_id } = request.params;
+      const { files } = request;
+
+      await this.addImagesToCarUseCase.execute({
+        car_id,
+        files: files.map(file => ({
+          name: file.name,
+          type: file.mimetype,
+          extension: String(file.name.split('.').pop()),
+          size: file.size,
+          content: file.buffer,
+        })),
+      });
+
+      return noContent();
+    } catch (error) {
+      if (error instanceof ValidationError) {
+        return badRequest(error);
+      }
+
+      if (error instanceof CarNotFoundWithProvidedIdError) {
+        return notFound(error);
+      }
+
+      throw error;
+    }
+  }
+}
+
+namespace AddImagesToCarController {
+  export type RequestParams = {
+    car_id: string;
+  };
+
+  export type Request = IHttpRequest<void, RequestParams, void, void>;
+
+  export type Response = IHttpResponse;
+}
+
+export { AddImagesToCarController };
