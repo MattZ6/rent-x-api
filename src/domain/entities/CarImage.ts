0d801b5e26a1e3c6f08acea9703c938baacd35f7
Matheus Felipe Zanin <matt_z6@hotmail.com>
2022-05-31 12:00:00 -0300

    feat(AddImagesToCar): add contract

---
+++ src/domain/entities/CarImage.ts
@@ -0,0 +1,10 @@
+export type CarImage = {
+  id: string;
+  car_id: string;
+  original_name: string;
+  mime_type: string;
+  extension: string;
+  size_in_bytes: number;
+  created_at: Date;
+  updated_at: Date;
+};
